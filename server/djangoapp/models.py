77d20f9bdccb991c936d0fd040bdec48c4fd7f39
JohanCabarcas <johan.cabarcas@outlook.com>
2021-06-01 09:31:48 +0000

    create car make and car_model models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,63 @@
-from django.db import models
-from django.utils.timezone import now
-
+#from django.db import models
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+#************************Settings*************************#
+import sys
+from django.utils.timezone import now
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
+
+from django.conf import settings
+import uuid
+
+# <HINT> Create a Car Make model `
+class CarMake(models.Model):
+    name_manufaturer = models.CharField(null=False, max_length=30, default='manufaturer')
+    description = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return "Manufacturer: " + self.name_manufaturer + "," + \
+               "Description: " + self.description
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+# <HINT> Create a Car Model model `
+class CarModel(models.Model):
+    # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 # - Name
+    car_model_name = models.CharField(null=False, max_length=30, default='car_model')
 # - Dealer id, used to refer a dealer created in cloudant database
+    dealer_id = models.IntegerField(default=0)
 # - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+
+    MODEL_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+    ]
+    choice = models.CharField(
+        null=False,
+        max_length=20,
+        choices=MODEL_CHOICES,
+        default=SEDAN
+    )
+
 # - Year (DateField)
+    year = models.DateField(null=True)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+    def __str__(self):
+        return "Manufacturer: " + self.car_make.name_manufaturer + "," + \
+                "Car model: " + self.car_model_name + "," + \
+                "Car choice: " + self.choice + "," + \
+                "Year: " + self.year
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
+
+#HINT: Configure the DEFAULT_AUTO_FIELD setting or the DjangoappConfig.default_auto_field attribute to point to a subclass of AutoField, e.g. 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
