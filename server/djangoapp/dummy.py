7ec0b96481e0f00f2e4bf15f4f1202a455985f99
JohanCabarcas <johan.cabarcas@outlook.com>
2021-06-05 15:19:03 +0000

    Create docker file in progress...

--- server/djangoapp/dummy.py
+++ server/djangoapp/dummy.py
@@ -1,32 +1,4 @@
-from models import CarDealer, DealerReview
+import os
+from pathlib import Path
 
-SEDAN = 'Sedan'
-SUV = 'SUV'
-WAGON = 'Wagon'
-example = (SEDAN, 'Sedan'),
-MODEL_CHOICES = [
-    (SEDAN, 'Sedan'),
-    (SUV, 'SUV'),
-    (WAGON, 'Wagon'),
-]
-
-print("type {}".format(type(example)))
-
-def car_model_getter():
-    car_model = CarModel()
-    return car_model
-
-car = car_model_getter
-
-print(car)
-
-    <!--Add review form here -->
-        <!--<form class="form-group" action="{% url 'djangoapp:add_review' dealer_id.dealer_id %}"  method="post">
-            <textarea class="form-control" id="content" name="content" rows="2" required></textarea>
-            <input class="form-check-input" type="checkbox" name="purchasecheck" id="purchasecheck"> 
-            <select name="car" id="car" class="form-select" required>
-                {% for car in cars %}
-                    <option selected value={{car.id}}>{{car.name}}-{{car.make.name}}-{{ car.year|date:"Y" }}</option>
-                {% endfor %}
-            </select>
-        </form>-->
+print("The path {}".format(Path(__file__).resolve().parent.parent))
