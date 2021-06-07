dcd9c7c01be0a5ebb88acbc1eaa42d1b1f299845
JohanCabarcas <johan.cabarcas@outlook.com>
2021-06-07 13:45:40 +0000

    add deployment yaml file

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -79,11 +79,10 @@ DATABASES = {
     #'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': BASE_DIR / 'db.sqlite3',
-    #,
+    #}
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
         'PASSWORD': 'NjE2My1qb2hhbmNh',
         'HOST': 'localhost',
         'PORT': '5432',
